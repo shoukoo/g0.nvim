3ef5cc1abe9d763abbcf47abee59867a480740fc
shoukoo <chenghunghsieh@Chengs-MacBook-Pro.local>
2023-09-20 22:56:29 +1000

    add utils

--- lua/g0/utils.lua
+++ lua/g0/utils.lua
@@ -26,4 +26,33 @@ M.extension= function()
   return ''
 end
 
+M.handle_job_data = function(data)
+  if not data then
+    return nil
+  end
+  -- Because the nvim.stdout's data will have an extra empty line at end on some OS (e.g. maxOS), we should remove it.
+  for _ = 1, 3, 1 do
+    if data[#data] == '' then
+      table.remove(data, #data)
+    end
+  end
+  if #data < 1 then
+    return nil
+  end
+  -- remove ansi escape code
+  for i, v in ipairs(data) do
+    data[i] = M.remove_ansi_escape(data[i])
+  end
+
+  return data
+end
+
+M.remove_ansi_escape = function(str)
+  local ansi_escape_pattern = '\27%[%d+;%d*;%d*m'
+  -- Replace all occurrences of the pattern with an empty string
+  str = str:gsub(ansi_escape_pattern, '')
+  str = str:gsub('\27%[[%d;]*%a', '')
+  return str
+end
+
 return M
