13f0c3f960a52fb8d8a1ead4443589ff0658ad74
shoukoo <chenghunghsieh@Chengs-MacBook-Pro.local>
2023-09-21 07:19:24 +1000

    rework format

--- lua/g0/format.lua
+++ lua/g0/format.lua
@@ -1,4 +1,5 @@
 local M = {}
+local utils = require"g0.utils"
 
 M.goimports = function()
   require('g0.install').install("goimports")
@@ -9,14 +10,27 @@ M.goimports = function()
     vim.cmd('write')
   end
 
-  local cmd = { 'goimports', '-w', '-l', vim.api.nvim_buf_get_name(buf) }
+  local cmd = { 'goimports', vim.api.nvim_buf_get_name(buf) }
   local job_id = vim.fn.jobstart(cmd, {
-    on_exit = function(_, exit_code)
-      if exit_code == 0 then
-        -- reload the buffer if goimports ran successfully
-        vim.cmd('edit')
+    on_stdout = function(_, data, _)
+      data = utils.handle_job_data(data)
+      if not data then
+        return
+      end
+      vim.notify('updating..' , vim.log.levels.INFO)
+      vim.api.nvim_buf_set_lines(0, 0, -1, false, data)
+      vim.cmd("edit")
+    end,
+    on_stderr = function(_, data, _)
+      data = utils.handle_job_data(data)
+      if data then
+        return vim.notify('goimports failed ' .. vim.inspect(data), vim.log.levels.ERROR)
       end
     end,
+    on_exit = function(_, exit_code)
+    end,
+    stdout_buffered = true,
+    stderr_buffered = true,
   })
 
   if job_id <= 0 then
@@ -26,4 +40,3 @@ M.goimports = function()
 end
 
 return M
-
