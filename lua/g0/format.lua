6d23d67763308cf801d3563400684e3f701d113e
shoukoo <chenghunghsieh@Chengs-MacBook-Pro.local>
2023-09-24 21:13:35 +1000

    run goimports in buffer

--- lua/g0/format.lua
+++ lua/g0/format.lua
@@ -1,25 +1,26 @@
 local M = {}
+local api = vim.api
+local fn= vim.fn
 local utils = require"g0.utils"
 
 M.goimports = function()
   require('g0.install').install("goimports")
 
-  local buf = vim.api.nvim_get_current_buf()
+  local buf = api.nvim_get_current_buf()
+
   -- if the change is not saved or a new unsaved file then call write
-  if vim.fn.getbufinfo('%')[1].changed == 1 then
+  if fn.getbufinfo('%')[1].changed == 1 then
     vim.cmd('write')
   end
 
   local cmd = { 'goimports', vim.api.nvim_buf_get_name(buf) }
-  local job_id = vim.fn.jobstart(cmd, {
+  local job_id = fn.jobstart(cmd, {
     on_stdout = function(_, data, _)
       data = utils.handle_job_data(data)
       if not data then
         return
       end
-      vim.notify('updating..' , vim.log.levels.INFO)
-      vim.api.nvim_buf_set_lines(0, 0, -1, false, data)
-      vim.cmd("edit")
+      api.nvim_buf_set_lines(0, 0, -1, false, data)
     end,
     on_stderr = function(_, data, _)
       data = utils.handle_job_data(data)
@@ -28,6 +29,7 @@ M.goimports = function()
       end
     end,
     on_exit = function(_, exit_code)
+      print("end".. exit_code)
     end,
     stdout_buffered = true,
     stderr_buffered = true,
