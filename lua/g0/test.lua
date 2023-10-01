f20e2eae05da8d46ff1306260a65f338360d62b8
shoukoo <chenghunghsieh@Chengs-MacBook-Pro.local>
2023-10-02 08:50:03 +1100

    debug

--- lua/g0/test.lua
+++ lua/g0/test.lua
@@ -28,6 +28,44 @@ M.test_current_dir = function()
   -- Run the "go test ./..." command in the specified directory
   local current_directory = fn.expand('%:p:h')
   local command = "cd " .. current_directory .. " && go test ./..."
+  print(command)
+
+  vim.cmd("term " .. command)
+  api.nvim_win_set_cursor(win_id, { fn.line('$'), 0 })
+
+  -- Example: Close the floating window when pressing 'q'
+  api.nvim_buf_set_keymap(buf, 'n', 'q', ':lua vim.api.nvim_win_close(' .. win_id .. ', true)<CR>', {
+    noremap = true,
+    silent = true,
+  })
+
+end
+
+M.test_current = function()
+
+  local buf = api.nvim_create_buf(false, true) -- Create a new buffer
+
+  local width = math.floor(o.columns * 0.5) -- 50% of the current window width
+  local height = math.floor(o.lines * 0.5)
+  local row = math.floor((o.lines - height) / 2)
+  local col = math.floor((o.columns - width) / 2)
+
+  -- Create the floating window
+  local win_id = api.nvim_open_win(buf, true, {
+    relative = 'editor',
+    width = width,
+    height = height,
+    col = col,
+    row = row,
+    style = 'minimal',
+    zindex = 250,
+    border = "single",
+    title = "press q to quit",
+  })
+
+  -- Run the "go test ./..." command in the specified directory
+  local current_directory = fn.expand('%:p:h')
+  local command = "cd " .. current_directory .. " && go test ./..."
 
   vim.cmd("term " .. command)
   api.nvim_win_set_cursor(win_id, { fn.line('$'), 0 })
