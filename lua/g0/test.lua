d2a00264ec658a1abedbe1f20957c13a1888835a
shoukoo <chenghunghsieh@Chengs-MacBook-Pro.local>
2023-10-19 06:22:57 +1100

    working version3

--- lua/g0/test.lua
+++ lua/g0/test.lua
@@ -52,12 +52,41 @@ M.test_current = function()
     node = node:parent()
   end
 
+
   if node then
     local buffer_name = fn.bufname('%') -- Get the full path of the current buffer
     local current_directory = fn.fnamemodify(buffer_name, ':h') -- Get the directory part
     local function_name = vim.treesitter.get_node_text(node:child(1), 0)
-    local command = "cd " .. current_directory .. " && go test -u " .. function_name
-    vim.notify(command, vim.log.levels.INFO)
+    local command = "cd " .. current_directory .. " && go test -run " .. function_name
+
+
+    local buf = api.nvim_create_buf(false, true) -- Create a new buffer
+    local width = math.floor(o.columns * 0.5) -- 50% of the current window width
+    local height = math.floor(o.lines * 0.5)
+    local row = math.floor((o.lines - height) / 2)
+    local col = math.floor((o.columns - width) / 2)
+    -- Create the floating window
+    local win_id = api.nvim_open_win(buf, true, {
+      relative = 'editor',
+      width = width,
+      height = height,
+      col = col,
+      row = row,
+      style = 'minimal',
+      zindex = 250,
+      border = "single",
+      title = "press q to quit",
+    })
+
+
+    vim.cmd("term " .. command)
+    api.nvim_win_set_cursor(win_id, { fn.line('$'), 0 })
+
+    -- Close the floating window when pressing 'q'
+    api.nvim_buf_set_keymap(buf, 'n', 'q', ':lua vim.api.nvim_win_close(' .. win_id .. ', true)<CR>', {
+      noremap = true,
+      silent = true,
+    })
   else
     print("Not inside a function")
   end
