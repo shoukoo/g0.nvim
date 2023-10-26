c27d1a2b07491a994a85d774d025f03540ed009d
shoukoo <andyhsieh@duck.com>
2023-10-27 05:23:50 +1100

    chore: rename file

--- lua/g0/modifytags.lua
+++ lua/g0/modifytags.lua
@@ -1,7 +1,8 @@
 local utils = require("g0.utils")
 
 local M = {}
-
+-- add json tag to all structs on that file
+-- gomodifytags -file api/queries_pr.go -all -add-tags=json
 M.go_add_tags = function ()
 end
 
