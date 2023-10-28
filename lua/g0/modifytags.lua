d57cb6b0b6ef674a3847cec89775e0bd6643cb07
shoukoo <andyhsieh@duck.com>
2023-10-29 06:04:22 +1100

    doc: useful note on modify tags

--- lua/g0/modifytags.lua
+++ lua/g0/modifytags.lua
@@ -3,6 +3,10 @@ local utils = require("g0.utils")
 local M = {}
 -- add json tag to all structs on that file
 -- gomodifytags -file api/queries_pr.go -all -add-tags=json
+-- add json tag based on the lines
+-- gomodifytags -file api/queries_pr.go -line=16,19 -add-tags=jso
+-- add json tag based on offset(bytes), this will transform the entire struct
+-- gomodifytags -file api/queries_pr.go -offset=520 -add-tags=json
 M.go_add_tags = function ()
 end
 
