a1e0e4dc9ef7533da088cfe2c7e88eb9d8bd6492
shoukoo <andyhsieh@duck.com>
2023-10-31 05:09:45 +1100

    wip: allow range and args in user cmd

--- lua/g0/modifytags.lua
+++ lua/g0/modifytags.lua
@@ -7,10 +7,31 @@ local M = {}
 -- gomodifytags -file api/queries_pr.go -line=16,19 -add-tags=jso
 -- add json tag based on offset(bytes), this will transform the entire struct
 -- gomodifytags -file api/queries_pr.go -offset=520 -add-tags=json
-M.go_add_tags = function ()
+M.add_tags = function(...)
+  P(...)
+  local cmd = vim.fn.getcmdline()
+  print("hello")
+  print(cmd)
+  -- local current_mode = vim.fn.mode()
+  local current_mode = vim.fn.visualmode('v')
+  print(current_mode)
+  -- Get the start and end lines of the visual selection
+  local start_line = vim.fn.line("'<") -- Start line
+  local end_line = vim.fn.line("'>") -- End line
+  local current_line = vim.fn.line('.')
+  print(start_line)
+  print(end_line)
+  print(current_line)
+
+  -- Iterate over the lines in the range and do something
+  -- for line = start_line, end_line do
+  --   -- Your logic here, using 'line' to access individual lines
+  --   -- For example, you can use vim.fn.getline(line) to get the content of the line
+  --   print("Line " .. line .. ": " .. vim.fn.getline(line))
+  -- end
 end
 
-M.go_remove_tag= function ()
+M.remove_tag = function()
 end
 
 return M
