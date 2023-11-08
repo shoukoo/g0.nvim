6bd7388e8491eeaaa11c7ac8fff9f42496194ce8
shoukoo <andyhsieh@duck.com>
2023-11-09 05:09:31 +1100

    wip: add G0RemoveTags cmd

--- lua/g0/commands.lua
+++ lua/g0/commands.lua
@@ -39,6 +39,7 @@ M.add_cmds = function()
   end)
 
   create_visual_command('G0AddTags', "require('g0.modifytags').add_tags('<args>')")
+  create_visual_command('G0RemoveTags', "require('g0.modifytags').remove_tags('<args>')")
 end
 
 return M
