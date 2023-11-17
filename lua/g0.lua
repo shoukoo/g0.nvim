39867513dfd3d68276be9b3e643366dc86901677
shoukoo <andyhsieh@duck.com>
2023-11-18 07:11:04 +1100

    feature: pass config in setup

--- lua/g0.lua
+++ lua/g0.lua
@@ -1,7 +1,8 @@
 local M = {}
 
-M.setup = function()
-  require("g0.commands").add_cmds()
+M.setup = function(opts)
+  local config = require('g0.config').merge(opts)
+  require("g0.commands").add_cmds(config)
 end
 
 return M
