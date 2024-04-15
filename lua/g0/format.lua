645cb82812ff45a9527da006cb7ad4e7509c2dc7
shoukoo <andyhsieh@duck.com>
2024-04-15 22:39:42 +1000

    feature: add lsp_imports

--- lua/g0/format.lua
+++ lua/g0/format.lua
@@ -40,4 +40,59 @@ M.goimports = function(config)
 
 end
 
+M.lsp_format = function()
+  vim.lsp.buf.format({
+    bufnr = vim.api.nvim_get_current_buf(),
+    name = 'gopls',
+  })
+  if vim.fn.getbufinfo('%')[1].changed == 1 then
+    vim.cmd('noautocmd write')
+  end
+end
+
+M.lsp_imports = function(config)
+  config = config or require("g0.config").defaults
+
+  local params = vim.lsp.util.make_range_params()
+  params.context = { only = { "source.organizeImports" } }
+
+  local gopls = M.client()
+  print(gopls)
+  if gopls == nil then
+    vim.notify('gopls not found', vim.log.levels.ERROR)
+    return
+  end
+  --
+  -- buf_request_sync defaults to a 1000ms timeout. Depending on your
+  -- machine and codebase, you may want longer. Add an additional
+  -- argument after params if you find that you have to write the file
+  -- twice for changes to be saved.
+  -- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+  local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, config.timeout)
+  vim.notify("Error: goimports timeout", vim.inspect(result), vim.log.levels.ERROR)
+
+  for cid, res in pairs(result or {}) do
+    for _, r in pairs(res.result or {}) do
+      if r.edit then
+        local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+        vim.lsp.util.apply_workspace_edit(r.edit, enc)
+      end
+    end
+  end
+  vim.lsp.buf.format({ async = false })
+
+  if vim.fn.getbufinfo('%')[1].changed == 1 then
+    vim.cmd('noautocmd write')
+  end
+end
+
+
+M.client = function()
+  local clients = vim.lsp.get_active_clients({
+    bufnr = vim.api.nvim_get_current_buf(),
+    name = 'gopls',
+  }) or {}
+  return clients[1]
+end
+
 return M
